4bb23a068ebf2494adc24d7ba35f0b20a52d27f8
bantioco <benjamin@antioco.fr>
2016-09-15 10:05:10 +0200

    Update

--- Resources/public/js/jquery.gitsync.js
+++ Resources/public/js/jquery.gitsync.js
@@ -1,6 +1,35 @@
 $(function(){
 
+	$( "li" ).each(function( index ) {
+		var inli = $( this ).text();
+		var n = inli.substr(2, 6)
+		
+		if (n.match("^commit")) {
+			$( this ).css({
+		      "font-weight": "bold",
+		      "font-size": "16px",
+		      "text-transform": "capitalize"
+		    });
+		}
 
-})
+		if (n.match("^Author")) {
+			$( this ).css({
+		      "font-weight": "300",
+		      "font-size": "14px",
+		      "color": "#555"
+		    });
+		}
+
+		if (n.match("^Date:")) {
+			$( this ).css({
+		      "font-weight": "300",
+		      "font-size": "11px",
+		      "color": "#BABABA"
+		    });
+		}
+
+	});
+
+});
 
 
